e20606f6a3a7b1f412f01277094267b86ac23fdc
amolsawle87 <68307196+amolsawle87@users.noreply.github.com>
2020-12-17 07:45:15 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 02:19:30 2020
+
+@author: Amol
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('knn_classifier.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+
+
+    return render_template('index.html', prediction_text='Note is $ {}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
